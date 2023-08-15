5c94061b698df01277b32b3d9393d5ba70ae2478
Pedro Enrique Perez Navarro <pedroeperezn@gmail.com>
2023-08-15 12:11:59 -0700

    Files are being shown on the app. also file duration

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,6 +32,7 @@
             FilesPreview = new ListView();
             textBox1 = new TextBox();
             folderBrowserAudio = new FolderBrowserDialog();
+            RefreshFilesButton = new Button();
             SuspendLayout();
             // 
             // BrowseFilesButton
@@ -40,32 +41,45 @@
             BrowseFilesButton.Name = "BrowseFilesButton";
             BrowseFilesButton.Size = new Size(139, 23);
             BrowseFilesButton.TabIndex = 0;
-            BrowseFilesButton.Text = "Browse for Files";
+            BrowseFilesButton.Text = "Browse for Directory";
             BrowseFilesButton.UseVisualStyleBackColor = true;
             BrowseFilesButton.Click += BrowseFilesButton_Click;
             // 
             // FilesPreview
             // 
+            FilesPreview.FullRowSelect = true;
             FilesPreview.Location = new Point(165, 52);
             FilesPreview.Name = "FilesPreview";
-            FilesPreview.Size = new Size(277, 287);
+            FilesPreview.Size = new Size(612, 287);
             FilesPreview.TabIndex = 1;
             FilesPreview.UseCompatibleStateImageBehavior = false;
             FilesPreview.View = View.Details;
+            FilesPreview.SelectedIndexChanged += FilesPreview_SelectedIndexChanged;
             // 
             // textBox1
             // 
             textBox1.Location = new Point(165, 18);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(277, 23);
+            textBox1.Size = new Size(612, 23);
             textBox1.TabIndex = 2;
             textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // RefreshFilesButton
+            // 
+            RefreshFilesButton.Location = new Point(12, 52);
+            RefreshFilesButton.Name = "RefreshFilesButton";
+            RefreshFilesButton.Size = new Size(139, 23);
+            RefreshFilesButton.TabIndex = 3;
+            RefreshFilesButton.Text = "Refresh Files";
+            RefreshFilesButton.UseVisualStyleBackColor = true;
+            RefreshFilesButton.Click += RefreshFilesButton_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(RefreshFilesButton);
             Controls.Add(textBox1);
             Controls.Add(FilesPreview);
             Controls.Add(BrowseFilesButton);
@@ -81,5 +95,6 @@
         private ListView FilesPreview;
         private TextBox textBox1;
         private FolderBrowserDialog folderBrowserAudio;
+        private Button RefreshFilesButton;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,5 @@
+using NAudio.Wave;
+
 namespace Pipelines_A3
 {
     public partial class Form1 : Form
@@ -5,6 +7,8 @@ namespace Pipelines_A3
         public Form1()
         {
             InitializeComponent();
+            FilesPreview.Columns.Add("File Name", 300);
+            FilesPreview.Columns.Add("File Length", 100);
         }
 
         private void BrowseFilesButton_Click(object sender, EventArgs e)
@@ -20,6 +24,58 @@ namespace Pipelines_A3
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RefreshFilesButton_Click(object sender, EventArgs e)
+        {
+            FilesPreview.Items.Clear();
+
+            string directory = folderBrowserAudio.SelectedPath;
+
+            if (Directory.Exists(directory))
+            {
+                var audioWAVFiles = Directory.GetFiles(directory, "*.wav");
+                var audioMP3Files = Directory.GetFiles(directory, "*.mp3");
+
+                TimeSpan duration;
+
+                FilesPreview.Items.Add("FOUND WAVE FILES: ");
+
+                foreach (var audioFile in audioWAVFiles)
+                {
+                    var wavItem = new ListViewItem(Path.GetFileName(audioFile));
+
+                    using (var reader = new WaveFileReader(audioFile))
+                    {
+                        duration = reader.TotalTime;
+                        wavItem.SubItems.Add(duration.ToString(@"hh\:mm\:ss"));
+                    }
+
+                    FilesPreview.Items.Add(wavItem);
+                    
+                }
+
+                FilesPreview.Items.Add(" ");
+                FilesPreview.Items.Add("FOUND MP3 FILES: ");
+
+                foreach (var audioFile in audioMP3Files)
+                {
+                    var mp3Item = new ListViewItem(Path.GetFileName(audioFile));
+
+                    using (var reader = new Mp3FileReader(audioFile))
+                    {
+                        duration = reader.TotalTime;
+                        mp3Item.SubItems.Add(duration.ToString(@"hh\:mm\:ss"));
+                    }
+
+                    FilesPreview.Items.Add(mp3Item);
+                }
+            }
+        }
+
+        private void FilesPreview_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
